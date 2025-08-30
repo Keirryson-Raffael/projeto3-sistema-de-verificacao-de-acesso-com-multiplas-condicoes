1df617c3338220ea137b916c197c6544ec765c95
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2025-08-30 16:24:27 -0300

    comentado Js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let nome = prompt("Digite seu nome completo: ").toLowerCase()
 let idade = parseInt(prompt("Digite sua idade: "))
 
-//  verificação
+//  verificação de nome e idade
 if (idade >= 18) {
     alert (`Olá, ${nome}! Você tem acesso completo. Boas-vindas à plataforma!`)
 } else if (nome == "admin") {
