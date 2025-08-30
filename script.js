0aa028212e1beedc282d2bf414b21956ea91fc55
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2025-08-30 16:08:11 -0300

    modificado script Js

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ if (idade >= 18) {
     alert (`Olá, ${nome}! Você tem acesso completo. Boas-vindas à plataforma!`)
 } else if (nome == "admin") {
     alert(`Bem-vindo, Admin! Acesso total liberado.`)
-} if (nome == "moderador" && idade >= 16) {
+} else if (nome == "moderador" && idade >= 16) {
     alert(`Olá, Moderador! Acesso liberado para a área de moderação.`)
-} else {
+} else if (nome == "moderador" && idade <= 16) {
     alert(`Acesso negado. O conteúdo é exclusivo para maiores de 18 anos.`)
-} if (idade >= 13 && idade <= 17) {
+} else if (idade >= 13 && idade <= 17) {
     alert(`Acesso negado. Você pode participar do nosso programa Jovem Aprendiz!`)
 } else if (idade <= 13) {
     alert(`Acesso negado. O conteúdo é exclusivo para maiores de 18 anos.`)
