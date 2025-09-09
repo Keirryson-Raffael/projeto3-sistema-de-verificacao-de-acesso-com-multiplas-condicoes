2293853305f2979cea24094a86df648738122852
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2025-09-09 09:40:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ let nome = prompt("Digite seu nome completo: ").toLowerCase()
 let idade = parseInt(prompt("Digite sua idade: "))
 
 //  verificação de nome e idade
-if (idade >= 18) {
-    alert (`Olá, ${nome}! Você tem acesso completo. Boas-vindas à plataforma!`)
-} else if (nome == "admin") {
-    alert(`Bem-vindo, Admin! Acesso total liberado.`)
-} else if (nome == "moderador" && idade >= 16) {
+if (nome === "admin") {
+    alert (`Bem-vindo, Admin! Acesso total liberado.`)
+} else if (idade >= 18) {
+    alert(`Olá, ${nome}! Você tem acesso completo. Boas-vindas à plataforma!`)
+} else if (nome === "moderador" && idade >= 16) {
     alert(`Olá, Moderador! Acesso liberado para a área de moderação.`)
 } else if (idade >= 13 && idade <= 17) {
     alert(`Acesso negado. Você pode participar do nosso programa Jovem Aprendiz!`)
 } else {
     alert("Acesso negado. O conteúdo é exclusivo para maiores de 18 anos.")
+
 }
